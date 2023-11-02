fb46cf0c81174a0be6b8f5efa3a5cd47e651d1e6
_OSHA_ <stiwwin@gmail.com>
2023-11-03 00:26:15 +0300

    Версия перед dm_chanel

--- main4.py
+++ main4.py
@@ -42,41 +42,43 @@ async def Intro_LS(user):
 
     #### 1 вопрос
     e_1 = embed.emb_2("Напиши свой никнейм в игре.")
-    await user.send(embed=e_1.emb_2)
+    await user.send(embed=e_1.emb)
     await question.q_1(user, bot)
     
-    #### 2 вопрос
+    #### 2 вопрос rdy 
     s.clear_items()
     e_2 = embed.emb_2("В какое время относительно МСК ты играешь в основном?")
     s.add_item(question.q_2())
-    await user.send(embed=e_2.emb_2, view = s)
+    await user.send(embed=e_2.emb, view = s)
+    # await bot.wait_for('on_interaction', check=None)
 
-
-    #### 3 вопрос    
+    #### 3 вопрос rdy
     s.clear_items()
     s.add_item(question.q_3())
     e_3 = embed.emb_2("Какой у тебя часовой пояс?")
     await user.send(embed=e_3.emb, view = s)
 
-    #### 4 вопрос
+    #### 4 вопрос rdy
     s.clear_items()
     s.add_item(question.q_4())
     e_4 = embed.emb_2("Сколько у тебя часов в SQUAD?")
     await user.send(embed=e_4.emb, view = s)
 
 
-    #### 5 вопрос    
+    #### 5 вопрос rdy
     s.clear_items()
+    s.add_item(question.q_5())
+    e_5 = embed.emb_2("Какое направление тебе нравится больше всего?")
+    await user.send(embed=e_5.emb, view = s)
 
-
-    #### 6 вопрос
+    #### 6 вопрос rdy
     s.clear_items()
     s.add_item(question.q_6())
     e_6 = embed.emb_2("Выбери одну роль (стрелковую специальность), номер один для тебя?")
     await user.send(embed=e_6.emb, view = s)
 
 
-    #### 7 вопрос
+    #### 7 вопрос rdy
     s.clear_items()
     s.add_item(question.q_7())
     e_7 = embed.emb_2("Выбери дополнительные 2 или более роли, помимо основной. Напиши их ниже.")
@@ -86,36 +88,45 @@ async def Intro_LS(user):
     #### 8 вопрос
     s.clear_items()
     e_8 = embed.emb_2("Ты понимаешь, что для того, что бы играть командно, нужно, что бы все делали одинаково?\nЭто \"одинаково\" - мы научим тебя делать, но не все может получатся сразу")
-    await user.send(embed=e_8.emb_2)
+    await user.send(embed=e_8.emb)
     await question.q_8(user, bot)
 
 
     #### 9 вопрос    
     s.clear_items()
     e_9 = embed.emb_2("Сколько тебе лет?")
-    await user.send(embed=e_9.emb_2)
+    await user.send(embed=e_9.emb)
     await question.q_9(user, bot)
 
-    #### 10 вопрос
+    #### 10 вопрос rdy
     s.clear_items()
+    e_10 = embed.emb_2("Оцени самостоятельно навык твоей стрельбы в SQUAD? от 0 до 10")
+    s.add_item(question.q_10())
+    await user.send(embed=e_10.emb, view = s)
 
 
-    #### 11 вопрос
+    #### 11 вопрос rdy
     s.clear_items()
+    e_11 = embed.emb_2("Насколько ты считаешь себя дисциплинированным игроком, если играешь в отряде? От 0 до 10")
+    s.add_item(question.q_11())
+    await user.send(embed=e_11.emb, view = s)
 
-
-    #### 12 вопрос
+    #### 12 вопрос rdy
     s.clear_items()
+    e_12 = embed.emb_2("Как ты считаешь, насколько ты хорош при радиообмене от 0 до 10?")
+    s.add_item(question.q_12())
+    await user.send(embed=e_12.emb, view = s)
 
-
-    #### 13 вопрос
+    #### 13 вопрос rdy
     s.clear_items()
-
+    e_13 = embed.emb_2("Ты хочешь играть серьезные игры в SQUAD? (Считай что это киберспорт, но только в скваде)")
+    s.add_item(question.q_13())
+    await user.send(embed=e_13.emb, view = s)
 
     #### 14 вопрос
     s.clear_items()
     e_14 = embed.emb_2("Откуда вы о нас узнали?\nЕсли вас пригласили, обязательно напишите ник человека, кто это сделал (Хотя бы примерный, мы поймем xD)")
-    await user.send(embed=e_14.emb_2)
+    await user.send(embed=e_14.emb)
     await question.q_14(user, bot)
 
 
@@ -135,6 +146,11 @@ async def on_ready():
     print("I am here")
     but_main = main_but()
     await channel.send("Я начал работу", view=but_main)
+
+@bot.event
+async def on_interaction(interaction):
+    print("on_interaction")
+        
 
 
 
--- question.py
+++ question.py
@@ -13,7 +13,7 @@ import numpy as np
 # class q_?(Select):
 #     def __init__(self): 
 #         super().__init__(
-#         placeholder="?", 
+#         placeholder="Список большой, листай вниз!", 
 #         options=[
 #         discord.SelectOption(label="0-50 часов", value="hours_1"),
 #         discord.SelectOption(label="50+ часов", value="hours_2")
@@ -24,19 +24,24 @@ import numpy as np
 #         print("Ответ?")
 
 
+# async def check(user):
+#     if user.id == message.author.id:
+#         return True
+
 
 ####### 1 вопрос #######
 async def q_1(user, bot):
-    mes = await bot.wait_for('message')
+    mes = await bot.wait_for('message', check=check(user))
+    #await bot.wait_for('message', check=check(user, mes))
     #print(f"Ваш никнейм : {mes.content}")
 
 
 ####### 2 вопрос #######
 class q_2(Select):
-    def __init__(self): 
+    def __init__(self):
         super().__init__(
-        placeholder="В какое время относительно МСК ты играешь в основном?", 
-        min_values = 2,
+        placeholder="Список большой, листай вниз!", 
+        min_values = 1,
         max_values = 4,
         options=[
         discord.SelectOption(label="Утром по МСК", value="prime_1"),
@@ -50,22 +55,23 @@ class q_2(Select):
         print("Ответ2")
 
 
+
 ####### 3 вопрос #######
 class q_3(Select):
     def __init__(self): 
         super().__init__(
-        placeholder="Какой у тебя часовой пояс?", 
+        placeholder="Список большой, листай вниз!", 
         options=[
-        discord.SelectOption(label="UTC-12:00 (Антивампирский остров)"),
-        discord.SelectOption(label="UTC-11:00 (Самоа)"),
-        discord.SelectOption(label="UTC-10:00 (Гавайи)"),
-        discord.SelectOption(label="UTC-09:00 (Аляска)"),
-        discord.SelectOption(label="UTC-08:00 (Тихоокеанское время - Северная Америка)"),
-        discord.SelectOption(label="UTC-07:00 (Горное время - Северная Америка)"),
-        discord.SelectOption(label="UTC-06:00 (Центральное время - Северная Америка)"),
-        discord.SelectOption(label="UTC-05:00 (Восточное время - Северная Америка)"),
-        discord.SelectOption(label="UTC-04:00 (Атлантическое время - Северная Америка)"),
-        discord.SelectOption(label="UTC-03:00 (Бразилиа, Буэнос-Айрес)"),
+        # discord.SelectOption(label="UTC-12:00 (Антивампирский остров)"),
+        # discord.SelectOption(label="UTC-11:00 (Самоа)"),
+        # discord.SelectOption(label="UTC-10:00 (Гавайи)"),
+        # discord.SelectOption(label="UTC-09:00 (Аляска)"),
+        # discord.SelectOption(label="UTC-08:00 (Тихоокеанское время - Северная Америка)"),
+        # discord.SelectOption(label="UTC-07:00 (Горное время - Северная Америка)"),
+        # discord.SelectOption(label="UTC-06:00 (Центральное время - Северная Америка)"),
+        # discord.SelectOption(label="UTC-05:00 (Восточное время - Северная Америка)"),
+        # discord.SelectOption(label="UTC-04:00 (Атлантическое время - Северная Америка)"),
+        # discord.SelectOption(label="UTC-03:00 (Бразилиа, Буэнос-Айрес)"),
         discord.SelectOption(label="UTC-02:00 (Среднеатлантическое время)"),
         discord.SelectOption(label="UTC-01:00 (Азорские острова, о. Зеленого Мыса)"),
         discord.SelectOption(label="UTC±00:00 (Западноевропейское время - Гринвич)"),
@@ -89,10 +95,10 @@ class q_3(Select):
         discord.SelectOption(label="UTC+10:30 (Лорд-Хаулендский остров)"),
         discord.SelectOption(label="UTC+11:00 (Соломоновы острова, Вануату)"),
         discord.SelectOption(label="UTC+11:30 (Норфолкское время)"),
-        discord.SelectOption(label="UTC+12:00 (Фиджи, Камчатское время)"),
-        discord.SelectOption(label="UTC+12:45 (Чатемское время)"),
-        discord.SelectOption(label="UTC+13:00 (Тонга)"),
-        discord.SelectOption(label="UTC+14:00 (Линия перемены даты)")
+        discord.SelectOption(label="UTC+12:00 (Фиджи, Камчатское время)")
+        # discord.SelectOption(label="UTC+12:45 (Чатемское время)"),
+        # discord.SelectOption(label="UTC+13:00 (Тонга)"),
+        # discord.SelectOption(label="UTC+14:00 (Линия перемены даты)")
         ], 
         custom_id="s_3")
     async def callback(self, interaction):
@@ -104,7 +110,7 @@ class q_3(Select):
 class q_4(Select):
     def __init__(self): 
         super().__init__(
-        placeholder="Сколько у тебя часов в SQUAD?", 
+        placeholder="Список большой, листай вниз!", 
         options=[
         discord.SelectOption(label="0-50 часов", value="hours_1"),
         discord.SelectOption(label="50+ часов", value="hours_2"),
@@ -119,67 +125,175 @@ class q_4(Select):
         await interaction.response.defer()
         print("Ответ4")
 
+
+####### 5 вопрос #######
+class q_5(Select):
+    def __init__(self): 
+        super().__init__(
+        placeholder="Список большой, листай вниз!", 
+        options=[
+        discord.SelectOption(label="CMD подразделение", value="podraz_1"),
+        discord.SelectOption(label="Атакующее подразделение", value="podraz_2"),
+        discord.SelectOption(label="ДРГ подразделение", value="podraz_3"),
+        discord.SelectOption(label="Оборонительное подразделение", value="podraz_4"),
+        discord.SelectOption(label="Стройбат подразделение", value="podraz_5"),
+        discord.SelectOption(label="Минометное подразделение", value="podraz_6"),
+        discord.SelectOption(label="Техническое подразделение", value="podraz_7"),
+        discord.SelectOption(label="Пилотное подразделение", value="podraz_8")
+        ], 
+        custom_id="s_5")
+    async def callback(self, interaction):
+        await interaction.response.defer()
+        print("Ответ5")
+
+
 ####### 6 вопрос #######
 class q_6(Select):
     def __init__(self): 
         super().__init__(
-        placeholder="Оснавная роль", 
+        placeholder="Список большой, листай вниз!", 
         options=[
-        discord.SelectOption(label="CMD", value="op1"),
-        discord.SelectOption(label="Сквадной", value="op2"),
-        discord.SelectOption(label="Лидер Фаер Тимы", value="op3"),
-        discord.SelectOption(label="Обычный стрелок", value="op4"),
-        discord.SelectOption(label="Стрелок ГП", value="op5"),
-        discord.SelectOption(label="Труба", value="op6"),
-        discord.SelectOption(label="Медик", value="op7"),
-        discord.SelectOption(label="Пулеметчик", value="op8"),
-        discord.SelectOption(label="Такнкист-командир", value="op9"),
-        discord.SelectOption(label="Танкист-водитель", value="op10"),
-        discord.SelectOption(label="Танкист-стрелок", value="op11"),
-        discord.SelectOption(label="Пилот", value="op12")
+        discord.SelectOption(label="CMD", value="osn_stg_cmd"),
+        discord.SelectOption(label="Сквадной", value="osn_stg_sl"),
+        discord.SelectOption(label="Лидер Фаер Тимы", value="osn_stg_flt"),
+        discord.SelectOption(label="Обычный стрелок", value="osn_stg_rfl"),
+        discord.SelectOption(label="Стрелок ГП", value="osn_stg_gl"),
+        discord.SelectOption(label="Труба", value="osn_stg_rpg"),
+        discord.SelectOption(label="Медик", value="osn_stg_med"),
+        discord.SelectOption(label="Пулеметчик", value="osn_stg_mach"),
+        discord.SelectOption(label="Такнкист-командир", value="osn_stg_tank_sl"),
+        discord.SelectOption(label="Танкист-водитель", value="osn_stg_tnak_mehv"),
+        discord.SelectOption(label="Танкист-стрелок", value="osn_stg_tank_bash"),
+        discord.SelectOption(label="Пилот", value="osn_stg_pilt")
         ], 
         custom_id="s_6")
     async def callback(self, interaction):
         await interaction.response.defer()
         print("Ответ6")
 
+
 ####### 7 вопрос
 class q_7(Select):
     def __init__(self): 
         super().__init__(
-        placeholder="Доп. Роли", 
+        placeholder="Список большой, листай вниз!", 
         min_values = 2,
         max_values = 12,
         options=[
-        discord.SelectOption(label="CMD", value="option1"),
-        discord.SelectOption(label="Сквадной", value="option2"),
-        discord.SelectOption(label="Лидер Фаер Тимы", value="option3"),
-        discord.SelectOption(label="Обычный стрелок", value="option4"),
-        discord.SelectOption(label="Стрелок ГП", value="option5"),
-        discord.SelectOption(label="Труба", value="option6"),
-        discord.SelectOption(label="Медик", value="option7"),
-        discord.SelectOption(label="Пулеметчик", value="option8"),
-        discord.SelectOption(label="Такнкист-командир", value="option9"),
-        discord.SelectOption(label="Танкист-водитель", value="option10"),
-        discord.SelectOption(label="Танкист-стрелок", value="option11"),
-        discord.SelectOption(label="Пилот", value="option12")
+        discord.SelectOption(label="CMD", value="dop_stg_cmd"),
+        discord.SelectOption(label="Сквадной", value="dop_stg_sl"),
+        discord.SelectOption(label="Лидер Фаер Тимы", value="dop_stg_flt"),
+        discord.SelectOption(label="Обычный стрелок", value="dop_stg_rfl"),
+        discord.SelectOption(label="Стрелок ГП", value="dop_stg_gl"),
+        discord.SelectOption(label="Труба", value="dop_stg_rpg"),
+        discord.SelectOption(label="Медик", value="dop_stg_med"),
+        discord.SelectOption(label="Пулеметчик", value="dop_stg_mach"),
+        discord.SelectOption(label="Такнкист-командир", value="dop_stg_tank_sl"),
+        discord.SelectOption(label="Танкист-водитель", value="dop_stg_tnak_mehv"),
+        discord.SelectOption(label="Танкист-стрелок", value="dop_stg_tank_bash"),
+        discord.SelectOption(label="Пилот", value="dop_stg_pilt")
         ], 
-        custom_id="s_4")
+        custom_id="s_7")
     async def callback(self, interaction):
         await interaction.response.defer()
         print("Ответ7")
 
 
-
 ####### 8 вопрос #######
 async def q_8(user, bot):
-    await user.send("Ты понимаешь, что для того, что бы играть командно, нужно, что бы все делали одинаково?")
     mes = await bot.wait_for('message')
-    print(f"Ваш ответ : {mes.content}")
 
 ####### 9 вопрос #######
 async def q_9(user, bot):
     mes = await bot.wait_for('message')
+
+
+####### 10 вопрос
+class q_10(Select):
+    def __init__(self): 
+        super().__init__(
+        placeholder="Список большой, листай вниз!", 
+        options=[
+        discord.SelectOption(label="0"),
+        discord.SelectOption(label="1"),
+        discord.SelectOption(label="2"),
+        discord.SelectOption(label="3"),
+        discord.SelectOption(label="4"),
+        discord.SelectOption(label="5"),
+        discord.SelectOption(label="6"),
+        discord.SelectOption(label="7"),
+        discord.SelectOption(label="8"),
+        discord.SelectOption(label="9"),
+        discord.SelectOption(label="10")
+        ], 
+        custom_id="s_10")
+    async def callback(self, interaction):
+        await interaction.response.defer()
+        print("Ответ10")
+
+
+####### 11 вопрос
+class q_11(Select):
+    def __init__(self): 
+        super().__init__(
+        placeholder="Список большой, листай вниз!", 
+        options=[
+        discord.SelectOption(label="0"),
+        discord.SelectOption(label="1"),
+        discord.SelectOption(label="2"),
+        discord.SelectOption(label="3"),
+        discord.SelectOption(label="4"),
+        discord.SelectOption(label="5"),
+        discord.SelectOption(label="6"),
+        discord.SelectOption(label="7"),
+        discord.SelectOption(label="8"),
+        discord.SelectOption(label="9"),
+        discord.SelectOption(label="10")
+        ], 
+        custom_id="s_11")
+    async def callback(self, interaction):
+        await interaction.response.defer()
+        print("Ответ11")
+
+
+####### 12 вопрос
+class q_12(Select):
+    def __init__(self): 
+        super().__init__(
+        placeholder="Список большой, листай вниз!", 
+        options=[
+        discord.SelectOption(label="0"),
+        discord.SelectOption(label="1"),
+        discord.SelectOption(label="2"),
+        discord.SelectOption(label="3"),
+        discord.SelectOption(label="4"),
+        discord.SelectOption(label="5"),
+        discord.SelectOption(label="6"),
+        discord.SelectOption(label="7"),
+        discord.SelectOption(label="8"),
+        discord.SelectOption(label="9"),
+        discord.SelectOption(label="10")
+        ], 
+        custom_id="s_12")
+    async def callback(self, interaction):
+        await interaction.response.defer()
+        print("Ответ12")
+
+
+####### 13 вопрос
+class q_13(Select):
+    def __init__(self): 
+        super().__init__(
+        placeholder="Список большой, листай вниз!", 
+        options=[
+        discord.SelectOption(label="Да"),
+        discord.SelectOption(label="Нет")
+        ], 
+        custom_id="s_13")
+    async def callback(self, interaction):
+        await interaction.response.defer()
+        print("Ответ13")
+
 
 ####### 14 вопрос #######
 async def q_14(user, bot):
